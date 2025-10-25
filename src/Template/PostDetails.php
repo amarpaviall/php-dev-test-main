95c7dae70d999bcb95b4c7b17a15110b2c79ab81
amarpaviall <er.amarpaviall@gmail.com>
2025-10-25 17:13:06 -0400

    Display post title and author name

--- src/Template/PostDetails.php
+++ src/Template/PostDetails.php
@@ -8,8 +8,13 @@ class PostDetails extends Layout
 {
     protected function renderPage(Context $context): string
     {
+        $title = htmlspecialchars($context->post->title);
+        $author = htmlspecialchars($context->post->author);
+
         return <<<HTML
             <p>SHOW CONTENT FOR {$context->content} HERE</p>
+            <h1>{$title}</h1>
+            <p><em>by {$author}</em></p>
             HTML;
     }
 }
