43c74617e11759830cae4213a91977f35f02ca1c
amarpaviall <er.amarpaviall@gmail.com>
2025-10-25 16:58:59 -0400

    css for title and count

--- src/Template/PostIndex.php
+++ src/Template/PostIndex.php
@@ -8,8 +8,8 @@ class PostIndex extends Layout
 {
     protected function renderPage(Context $context): string
     {
-        $html = "<h1>" . htmlspecialchars($context->title) . "</h1>";
-        $html .= "<span>" . "Total Posts : " . $context->content. "</span>";
+        $html = "<h1 class='page-title'>" . htmlspecialchars($context->title) . "</h1>";
+        $html .= "<span class='post-count'>" . "Total Posts : " . $context->content. "</span>";
         $html .= <<<HTML
 
             HTML;
