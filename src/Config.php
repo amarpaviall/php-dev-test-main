d218996ac77de93c7c46eb221f101cd8bbf407e3
amarpaviall <er.amarpaviall@gmail.com>
2025-10-25 15:21:29 -0400

    create mysql connection

--- src/Config.php
+++ src/Config.php
@@ -4,5 +4,11 @@ namespace silverorange\DevTest;
 
 class Config
 {
-    public string $dsn = 'pgsql:host=localhost;port=5532;dbname=silverorange;user=silverorange;password=silverorange';
+    //public string $dsn = 'pgsql:host=localhost;port=5532;dbname=silverorange;user=silverorange;password=silverorange';
+
+    // MySQL connection
+    public string $dsn = 'mysql:host=localhost;port=3306;dbname=silverorange;charset=utf8mb4';
+    public string $user = 'root';
+    public string $password = '';
+
 }
