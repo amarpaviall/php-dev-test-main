9944b30ee6a4e257ee499f5ce7acfd6c337024ee
amarpaviall <er.amarpaviall@gmail.com>
2025-10-25 16:39:44 -0400

    import post button and message text

--- src/Template/Root.php
+++ src/Template/Root.php
@@ -8,8 +8,25 @@ class Root extends Layout
 {
     protected function renderPage(Context $context): string
     {
+        $messageHtml = '';
+        if (!empty($context->message)) {
+            $messageHtml = <<<HTML
+                <div class="alert alert-success">
+                    {$context->message}
+                </div>
+            HTML;
+        }
+
         return <<<HTML
+
             <p>Things are up and running—you’ve got this!</p>
+            {$messageHtml}
+            <form method="post" action="/">
+                <button type="submit" name="import" value="1" class="button">
+                    Import Posts
+                </button>
+            </form>
+
             HTML;
     }
 }
