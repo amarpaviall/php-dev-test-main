f6cf9af928af2069776e5384aed05b83030508b5
amarpaviall <er.amarpaviall@gmail.com>
2025-10-25 17:03:13 -0400

    display post titles, post date and authors on main post page

--- src/Template/PostIndex.php
+++ src/Template/PostIndex.php
@@ -10,9 +10,31 @@ class PostIndex extends Layout
     {
         $html = "<h1 class='page-title'>" . htmlspecialchars($context->title) . "</h1>";
         $html .= "<span class='post-count'>" . "Total Posts : " . $context->content. "</span>";
-        $html .= <<<HTML
+        if (!empty($context->posts) && is_array($context->posts)) {
+            $html .= "<ul class='post-list'>";
 
-            HTML;
+            foreach ($context->posts as $post) {
+                if (!$post) continue;
+
+                $id = urlencode($post->id ?? '');
+                $title = htmlspecialchars($post->title ?? '');
+                $author = htmlspecialchars($post->author ?? '');
+                $date =  htmlspecialchars(date('F j, Y', strtotime($post->created_at) ?? ''));
+
+                $html .= <<<HTML
+                <li>
+                    <a href="/posts/{$id}">{$title}</a>
+                    <div class="post-meta">
+                        <span class="author">Author :  {$author}</span>
+                        <span class="date">Date : {$date}</span>
+                    </div>
+                </li>
+                HTML;
+            }
+                $html .= "</ul>";
+            } else {
+                $html .= "<p>No posts found.</p>";
+            }
         return $html;
     }
 }
